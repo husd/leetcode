eca6ca218763f86aec5db5daf8fc0cbaee66a89c
hushengdong <hushengdong@jd.com>
2019-10-30 16:40:56 +0800

    初始化代码结构。 迁移了3个算法。

---
+++ src/main/java/com/husd/leetcode/entry/ListNode.java
@@ -0,0 +1,16 @@
+package com.husd.leetcode.entry;
+
+/**
+ * 这里的注释内容，需要对类的使用做一个简单的说明。
+ *
+ * @author hushengdong
+ * @create 2019-10-30 16:32:00
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/husd/leetcode/entry/TreeNode.java
@@ -0,0 +1,16 @@
+package com.husd.leetcode.entry;
+
+/**
+ * 这里的注释内容，需要对类的使用做一个简单的说明。
+ *
+ * @author hushengdong
+ * @create 2019-10-30 16:35:00
+ */
+public class TreeNode {
+
+    public TreeNode root;
+
+    public TreeNode left;
+
+    public TreeNode right;
+}

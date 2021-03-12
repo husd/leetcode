f39a2b4757e1b8619f06185292b20f84d69a8e97
hushengdong <shengdonghu@126.com>
2021-03-12 15:25:44 +0800

    增加了2道题

---
+++ src/main/java/com/husd/leetcode/string/ReverseLeftWords.java
@@ -0,0 +1,53 @@
+package com.husd.leetcode.string;
+
+/**
+ * https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+ * <p>
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ * <p>
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 1 <= k < s.length <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author hushengdong
+ */
+public class ReverseLeftWords {
+
+    public String reverseLeftWords(String s, int n) {
+
+        int len = s.length();
+        n = n % len;
+        if (n == 0) {
+            return s;
+        }
+        StringBuilder res = new StringBuilder(len);
+        for (int i = n; i < len; i++) {
+            res.append(s.charAt(i));
+        }
+        for (int i = 0; i < n; i++) {
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+
+    public String reverseLeftWords2(String s, int n) {
+        return s.substring(n, s.length()) + s.substring(0, n);
+    }
+
+}
---
+++ src/main/java/com/husd/leetcode/string/ReverseWords.java
@@ -0,0 +1,108 @@
+package com.husd.leetcode.string;
+
+import java.util.*;
+
+/**
+ * 剑指 Offer 58 - I. 翻转单词顺序
+ * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。
+ * 例如输入字符串"I am a student. "，则输出"student. a am I"。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ * <p>
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ * <p>
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * 注意：本题与主站 151 题相同：https://leetcode-cn.com/problems/reverse-words-in-a-string/
+ *
+ * @author hushengdong
+ */
+public class ReverseWords {
+
+    public String reverseWords2(String s) {
+
+        if(s.equals("")) {
+            return "";
+        }
+        String[] arr = s.split(" ");
+        StringBuilder res = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        int c = 0;
+        for (String s1 : arr) {
+            if (s1.length() == 0) {
+                continue;
+            }
+            c++;
+            stack.push(s1);
+        }
+        while (c > 1) {
+            res.append(stack.pop());
+            res.append(" ");
+            c = c - 1;
+        }
+        if(!stack.isEmpty()) {
+            res.append(stack.pop());
+        }
+        return res.toString();
+    }
+
+    public String reverseWords3(String s) {
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+    public String reverseWords4(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') {
+            ++left;
+        }
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') {
+            --right;
+        }
+        //这是1个队列，可以插入到队列的头部，可以插入到队尾
+        Deque<String> d = new ArrayDeque<String>();
+        StringBuilder word = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if ((word.length() != 0) && (c == ' ')) {
+                // 将单词 push 到队列的头部
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+        return String.join(" ", d);
+    }
+
+    public static void main(String[] args) {
+
+        ReverseWords reverseWords = new ReverseWords();
+        System.out.println(reverseWords.reverseWords2("my name  is ok."));
+    }
+}
